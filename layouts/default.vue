8af9946871b94697e16807f6ccaf9e2a86d922d5
Robson Tenório <rrtenorio@gmail.com>
2019-05-31 13:51:05 -0300

    ..

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,31 +4,25 @@
       <v-container fill-height fluid class="pa-0">
         <v-layout child-flex>
           <!-- render pages -->
-          <nuxt />
+          <nuxt/>
         </v-layout>
       </v-container>
     </v-content>
-    <v-footer color="transparent" fixed class=" pb-3 pl-4 grey--text">
-      <span class="title pr-3">
-        <v-icon class="grey--text" medium>bug_report</v-icon> Smax bugs
+    <v-footer color="transparent" fixed class="pb-0 pl-4 grey--text title">
+      <span class="pr-3">
+        <v-icon class="grey--text" left medium>bug_report</v-icon>Smax bugs
       </span>
-      <span class="title pr-3">
-        <v-icon class="grey--text" medium>warning</v-icon> Sentry issues
+      <span class="pr-3">
+        <v-icon class="grey--text" left medium>warning</v-icon>Sentry issues
       </span>
-      <span class="title pr-3">
-        <v-icon class="grey--text" medium>track_changes</v-icon> Sonar issues
+      <span class="pr-3">
+        <v-icon class="grey--text" left medium>track_changes</v-icon>Sonar issues
       </span>
-      <span class="title pr-3">
-        <v-icon class="grey--text" medium>favorite</v-icon> Test coverage
+      <span class="pr-3">
+        <v-icon class="grey--text" left medium>favorite</v-icon>Test coverage
       </span>
-      <v-spacer />
-      <v-btn
-        flat
-        icon
-        large
-        @click="$router.push('settings')"
-        class="grey--text"
-      >
+      <v-spacer/>
+      <v-btn flat icon large @click="$router.push('settings')" class="grey--text">
         <v-icon class="grey--text">settings</v-icon>
       </v-btn>
     </v-footer>
